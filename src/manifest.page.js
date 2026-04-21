ecd6628d663618e8a7a2dfd5fcaf9c98973c007e
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-04-21 14:00:38 -0300

    fix: format manifest

---
+++ src/manifest.page.js
@@ -0,0 +1,23 @@
+export const lang = ["en", "fr"];
+export const en = { url: "/manifest-en.json" };
+export const fr = { url: "/manifest-fr.json" };
+
+export default (data) => JSON.stringify(
+	{
+		name: data.site[data.lang].name,
+		icons: [
+			{
+				src: "/icon-192.png",
+				type: "image/png",
+				sizes: "192x192",
+				purpose: "any maskable"
+			}, {
+				src: "/icon-512.png",
+				type: "image/png",
+				sizes: "512x512"
+			}
+		],
+	},
+	null,
+	2,
+);
