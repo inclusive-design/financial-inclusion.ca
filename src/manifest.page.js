a277b2056bc6bd491f63eeec9766455424c4039a
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-04-21 15:18:55 -0300

    fix: some minor tweaks

--- src/manifest.page.js
+++ src/manifest.page.js
@@ -2,22 +2,24 @@ export const lang = ["en", "fr"];
 export const en = { url: "/manifest-en.json" };
 export const fr = { url: "/manifest-fr.json" };
 
-export default (data) => JSON.stringify(
-	{
-		name: data.site[data.lang].name,
-		icons: [
-			{
-				src: "/icon-192.png",
-				type: "image/png",
-				sizes: "192x192",
-				purpose: "any maskable"
-			}, {
-				src: "/icon-512.png",
-				type: "image/png",
-				sizes: "512x512"
-			}
-		],
-	},
-	null,
-	2,
-);
+export default (data) =>
+  JSON.stringify(
+    {
+      name: data.site[data.lang].name,
+      icons: [
+        {
+          src: "/icon-192.png",
+          type: "image/png",
+          sizes: "192x192",
+          purpose: "any maskable",
+        },
+        {
+          src: "/icon-512.png",
+          type: "image/png",
+          sizes: "512x512",
+        },
+      ],
+    },
+    null,
+    2,
+  );
