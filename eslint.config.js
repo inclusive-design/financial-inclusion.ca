5bac98c18dc46c5267faf090fcff67c55fd1e2d9
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-06-22 15:58:24 -0300

    fix: resolve linting issues (#41)

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,15 @@
-import {defineConfig} from 'eslint/config';
+import { defineConfig } from 'eslint/config';
 import eslintConfigInclusiveDesign from '@inclusive-design/eslint-config';
 
 export default defineConfig([
 	{
 		extends: [eslintConfigInclusiveDesign],
-		ignores: ['_site/**'],
 		rules: {
-			camelcase: ['error', {properties: 'never'}],
+			camelcase: ['error', { properties: 'never' }],
+			'require-unicode-regexp': ['error', { requireFlag: 'u' }],
 		},
+	},
+	{
+		ignores: ['_site/**', 'README.md'],
 	},
 ]);
